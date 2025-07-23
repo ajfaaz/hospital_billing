797b9bdee0cdb42b88537b73298e333cb3d05b21
ajfaaz <ajmj05@gmail.com>
2025-07-23 11:43:31 +0100

    Fix: correct settings and Procfile for Heroku

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_billing.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
